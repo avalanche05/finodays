15ff4828dc97c6b937dc771553fec4e10ff4cb21
skewbekkebweks <93439487+skewbekkebweks@users.noreply.github.com>
2023-08-29 18:48:11 +0300

    email sender done

---
+++ server/utils/email_sender.py
@@ -0,0 +1,13 @@
+import smtplib, ssl
+
+port = 465
+smtp_server = "smtp.gmail.com"
+sender_email = "cfamarket.notifications@gmail.com"
+password = "tktcsiqnfcmbidvt"
+
+
+def send_email(receiver_email: str, message: str):
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
